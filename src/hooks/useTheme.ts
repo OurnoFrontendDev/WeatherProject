529692e75b8251ee5b39c439e8c35e01059b82da
OurnoFrontendDev <novikov99951@gmail.com>
2024-11-29 23:52:55 +0300

    1. Добавил пробелы 2. Переделал хук UseDebounce 3. Вынес запров в Сага 4. Вынес функцию конвертации времени в utils 5. Поправил типизацию 6. Исправил неймиг компонентов

--- src/hooks/useTheme.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext, ThemeContextType } from "../context/ThemeContext";
-
-export const useTheme = (): ThemeContextType => {
-  return useContext(ThemeContext);
-};
